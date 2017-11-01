c7d418a82ac86ae44816cd7541c9e45df6178a67
antschreier82 <as0858525@otc.edu>
2017-10-31 22:08:17 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue("hello world!"));
+console.log(chalk.green("There once was a man from peru"));
+console.log(chalk.red("Who dreamnt he was eating his shoe"));
+console.log(chalk.yellow("He awoke, with a fright"));
+console.log(chalk.white("In the middle of the night"));
+console.log(chalk.magenta("To find out his dream"));
+console.log(chalk.cyanBright("Had come true."));
